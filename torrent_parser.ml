b9e4ab16090424e78e3f6f6094bb4d5ae940d8cf
Boris Berger <berger.boris@orange.fr>
2015-02-17 19:39:03 +0100

    gitignore + functional get

--- torrent_parser.ml
+++ torrent_parser.ml
@@ -1,12 +1,11 @@
 open Core.Std
 open Async.Std
 
-let url_setup = Uri.make 
-    ~host:"http://cdimage.debian.org/"
-    ~path:"debian-cd/7.8.0/amd64/bt-dvd/debian-7.8.0-amd64-DVD-1.iso.torrent"
+let url_setup = Uri.make ~scheme:"http" ~host:"cdimage.debian.org"
+    ~path:"/debian-cd/7.8.0/amd64/bt-dvd/debian-7.8.0-amd64-DVD-1.iso.torrent"
 
 let url = url_setup()
 
-let req = Cohttp.Request.make ~meth:`GET url
+let get = Cohttp_lwt_unix.Client.get url
 
 
