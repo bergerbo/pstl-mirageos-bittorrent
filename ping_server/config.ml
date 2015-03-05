223f6928b2405986ab06573b4c471858536cfb18
Boris Berger <berger.boris@orange.fr>
2015-03-05 22:31:25 +0100

    Added ping server, build passes but runtime crahes on my machine

---
+++ ping_server/config.ml
@@ -0,0 +1,8 @@
+open Mirage
+
+let main = foreign "Unikernel.Main" (console @-> network @-> job)
+
+let () =
+    add_to_opam_packages [ "tcpip" ];
+    add_to_ocamlfind_libraries [ "tcpip.ethif"; "tcpip.ipv4" ];
+    register "ping" [ main $ default_console $ tap0 ]
