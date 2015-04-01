85462930040e67e59583d9b60151930b83d8a189
Boris Berger <berger.boris@orange.fr>
2015-04-01 18:03:49 +0200

    Fixed resolver problem

--- config.ml
+++ config.ml
@@ -1,12 +1,12 @@
 open Mirage
 
-let net = `Direct
+let net = `Socket
     (*try match Sys.getenv "NET" with
     | "direct" -> `Direct
     | _ -> `Socket
     with Not_found -> `Direct*)
 
-let dhcp = `Static
+let dhcp = `Dhcp
     (*try match Sys.getenv "DHCP" with
     | "0" -> `Static
     | _ -> `Dhcp
@@ -19,13 +19,15 @@ let stack console =
     | `Socket, _ -> socket_stackv4 console [Ipaddr.V4.any]
 
 let main =
-    foreign "Torrent_parser.Main" @@ console @-> fs  @-> conduit @-> job
+    foreign "Torrent_parser.Main" @@
+        console @-> fs @-> resolver @-> conduit @-> job
 
 let () =
     add_to_ocamlfind_libraries ["mirage-http"];
     add_to_opam_packages ["mirage-http"];
     let sv4 = stack default_console in
+    let res_dns = resolver_dns sv4 in
     let conduit = conduit_direct sv4 in
     let fat_fs = fat(block_of_file "disk.img") in 
-    let job = [main $ default_console $ fat_fs $ conduit ] in
+    let job = [main $ default_console $ fat_fs $ res_dns $  conduit ] in
     register "main" job
--- torrent_parser.ml
+++ torrent_parser.ml
@@ -3,41 +3,35 @@ open Cohttp_lwt_unix
 open Printf
 open V1_LWT
 
-let url = Uri.make 
-  ~scheme:"http" 
-  ~host:"cdimage.debian.org"
-  ~path:"/debian-cd/7.8.0/amd64/bt-dvd/debian-7.8.0-amd64-DVD-1.iso.torrent" 
-  ()
 
-(* we ignore the error code. *)
-  
 module Main (C: CONSOLE)
             (F: sig include FS with type
                 page_aligned_buffer = Cstruct.t
             end)
-            (CON: Conduit_mirage.S )
-            = struct
+            (RES:Resolver_lwt.S)
+            (CON: Conduit_mirage.S ) = struct
 
     module HTTP = HTTP.Make(CON)
 
     type suite = {
         c : C.t;
         fs : F.t;
-        con : CON.ctx;
+        res : Resolver_lwt.t;
+        ctx : CON.ctx;
     }
+    let url = Uri.of_string
+"http://cdimage.debian.org/debian-cd/current-live/amd64/bt-hybrid/debian-live-7.8.0-amd64-standard.iso.torrent"
 
     let write_torrent suite s path =
         let fs = suite.fs
         and c = suite.c in
         F.create fs path >>= function
-        | `Error e -> fail (Failure ("Error writing"))
+        | `Error e -> fail (Failure ("Error creating file"))
         | `Ok _ ->
         let bytes = Cstruct.create (String.length s) in
         for i = 0 to (String.length s) - 1 do
-            C.log c (sprintf "%d %c \n" i s.[i]);
             Cstruct.set_char bytes i s.[i]
         done;
-        C.log c (sprintf "%d\n" (String.length s));
         (F.write fs path 0 bytes) >>= function
         | `Error e -> fail (Failure ("Error writing"))
         | `Ok _ -> C.log c "Done"; return ()
@@ -56,31 +50,32 @@ module Main (C: CONSOLE)
 
     let get_torrent suite url path=
         let fs = suite.fs
-        and c = suite.c in
-        let p =
-        HTTP.Client.get url >>= fun (r,b) ->
+        and c = suite.c 
+        and ctx = suite.ctx 
+        and resolver = suite.res in
+        let ctx = { HTTP.Net_IO.resolver; ctx} in
+        HTTP.Client.get ~ctx  url >>= fun (r,b) ->
         match r.Cohttp.Response.status with
-            | `OK | `Code 200 ->
-            Cohttp_lwt_body.to_string b >>= fun s ->
-            (*write_torrent suite s path >>
-            let torrent = Torrent.create_from_data s in
-            Torrent.print torrent;*)
-            return ()
-            | _ ->
-            let open Cohttp.Code in
-            let open Printf in
-            let s = string_of_status r.Cohttp.Response.status in
-            Printf.eprintf "log: %s\n" s;
-            return ()
-        in p 
+        | `OK | `Code 200 ->
+        Cohttp_lwt_body.to_string b >>= fun s ->
+        write_torrent suite s path >>
+        let torrent = Torrent.create_from_data s in
+        Torrent.print torrent;
+        return ()
+        | _ ->
+        let open Cohttp.Code in
+        let open Printf in
+        let s = string_of_status r.Cohttp.Response.status in
+        Printf.eprintf "log: %s\n" s;
+        return ()
 
-    let start c fs con =
-        let suite = { c; fs; con} in 
+    let start c fs res ctx =
+        let suite = { c; fs; res;  ctx} in 
         F.mkdir fs "torrents" >>
         let path = "torrents/test.torrent" in
-        get_torrent suite url path >>(*
+        get_torrent suite url path >>
         open_torrent suite path >>= fun torrent ->
-        Torrent.print torrent;*)
+        Torrent.print torrent;
         return ()
 
     (*
